b4e4db9de321140decf01cdf84bca88cd9936189
Miguel-Fr <miguel_franca@outlook.com>
2019-06-29 17:32:21 +0100

    #include libraries

--- Game Framework/sfml.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "headers/sfml/Event.h"
-#include "headers/sfml/Mouse.h"
-#include "headers/sfml/Shapes.h"
-#include "headers/sfml/Text.h"
-#include "headers/sfml/ToggleKey.h"
