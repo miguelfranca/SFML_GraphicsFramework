14b4f8e458c9ec444b549f683b63911fa302621c
Miguel-Fr <miguel_franca@outlook.com>
2019-06-29 17:34:26 +0100

    Grouping

---
+++ Game Framework/sfml.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "headers/sfml/Event.h"
+#include "headers/sfml/Mouse.h"
+#include "headers/sfml/Shapes.h"
+#include "headers/sfml/Text.h"
+#include "headers/sfml/ToggleKey.h"
