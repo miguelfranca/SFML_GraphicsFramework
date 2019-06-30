a3bc63eff08de9571e375d0647852edb1e87b47b
Miguel-Fr <miguel_franca@outlook.com>
2019-06-30 15:18:33 +0100

    Added Slider with template base classes

--- Game Framework/headers/gui/Slider.h
+++ Game Framework/headers/gui/Slider.h
@@ -8,18 +8,35 @@ namespace GF {
 	class Slider : public Widget {
 	public:
 	
-		Slider(float size, sf::Vector2f pos = CENTER_WINDOW, std::string orientacion = VERTICAL);
-		Slider(sf::Vector2f size, sf::Vector2f pos = CENTER_WINDOW, std::string orientacion = VERTICAL);
+		Slider(const float size, const sf::Vector2f pos = CENTER_WINDOW, const std::string orientacion = VERTICAL, const double max = 0, const double min = 0);
+		Slider(const sf::Vector2f size, const sf::Vector2f pos = CENTER_WINDOW, const std::string orientacion = VERTICAL, const double max = 0, const double min = 0);
 
-		void handleEvent(const GF::Event& event, const sf::RenderWindow& window) override {}
+		void handleEvent(const GF::Event& event, const sf::RenderWindow& window) override;
 
-		void draw(sf::RenderTarget& renderer, const sf::RenderStates state) const override {}
+		void draw(sf::RenderTarget& renderer, const sf::RenderStates state) const override;
+
+		template<typename T2>
+		T2 getValue() const {
+			// calculates a fraction of the established limits
+			if (this->orientacion == VERTICAL)
+				return ((1 + ((box.getPosition().y - box.getSize().y / 2) - slider.getPosition().y) / box.getSize().y) * (max - min)) + min;
+			if (this->orientacion == HORIZONTAL)
+				return ((1 + ((box.getPosition().x - box.getSize().x / 2) - slider.getPosition().x) / box.getSize().x) * (max - min)) + min;
+
+			return 0;
+		}
+
+		void setLimits(const double mmax, const double mmin) {
+			max = mmax;
+			min = mmin;
+		}
 
 	private:
-		/*GF::Button<> arrow_up;
-		GF::Button<> arrow_down;
-		GF::Button<T> slider;
+		T slider;
 		GF::Rectangle box;
-		GF::Line line;*/
+		GF::Line line;
+
+		std::string orientacion;
+		double max, min;
 	};
 }
--- Game Framework/source/gui/Slider.cpp
+++ Game Framework/source/gui/Slider.cpp
@@ -1,11 +1,84 @@
 #include "../../headers/gui/Slider.h"
 
 namespace GF {
-	Slider<GF::Circle>::Slider(float size, sf::Vector2f pos, std::string orientacion){
-		
+	Slider<GF::Circle>::Slider(const float size, const sf::Vector2f pos, const std::string o, const double mmax, const double mmin)
+	{
+		orientacion = o;
+		slider.setup(7, pos, sf::Color(150, 150, 150), sf::Color::White, 1);
+
+		if (orientacion == HORIZONTAL) 
+		{
+			line = GF::Line(sf::Vector2f(pos.x - size / 2, pos.y), sf::Vector2f(pos.x + size / 2, pos.y));
+			box = GF::Rectangle(sf::Vector2f(size, 40), pos, CENTERED);
+		}
+		if (orientacion == VERTICAL)
+		{
+			line = GF::Line(sf::Vector2f(pos.x, pos.y - size / 2), sf::Vector2f(pos.x, pos.y + size / 2));
+			box = GF::Rectangle(sf::Vector2f(40, size), pos, CENTERED);
+		}
+		max = mmax;
+		min = mmin;
 	}
 
-	Slider<GF::Rectangle>::Slider(sf::Vector2f size, sf::Vector2f pos, std::string orientacion){
+	Slider<GF::Rectangle>::Slider(const sf::Vector2f size, const sf::Vector2f pos, const std::string o, const double mmax, const double mmin)
+	{
+		orientacion = o;
+		max = mmax;
+		min = mmin;
+		box = GF::Rectangle(size, pos, CENTERED, sf::Color::Transparent, sf::Color::White, 1);
 
+		static const float side = 10.f;
+
+		if (orientacion == HORIZONTAL)
+		{
+			slider.setup(sf::Vector2f(side, size.y), pos, sf::Vector2f(side / 2, size.y / 2), sf::Color::White, sf::Color::Transparent, 0);
+		}
+		if (orientacion == VERTICAL)
+		{
+			slider.setup(sf::Vector2f(size.x, side), pos, sf::Vector2f(size.x / 2, side / 2), sf::Color::White, sf::Color::Transparent, 0);
+		}
+	}
+
+	template<class T2>
+	void Slider<T2>::handleEvent(const GF::Event& event, const sf::RenderWindow& window)
+	{
+		static const float factor = -3.f;
+		if (orientacion == VERTICAL)
+		{
+			if (box.isRolledOn(window)) {
+				if (GF::Mouse::Left.isPressed()) { // clicking inside the box sets the slider to the mouse position
+					slider.setPosition(slider.getPosition().x, GF::Mouse::getPosition(window).y);
+				}
+				if (event.type == GF::Event::MouseWheelMoved) { // scrolling with the mouse wheel moves the slider
+					if (box.getGlobalBounds().contains({ slider.getPosition().x, slider.getPosition().y + event.mouseWheel.delta * factor }))
+						slider.move(0, event.mouseWheel.delta * factor);
+				}
+			}
+		}
+
+		// same for horizontal, but the coordinates are switched
+		if (orientacion == HORIZONTAL)
+		{
+			if (box.isRolledOn(window)) {
+				if (GF::Mouse::Left.isPressed()) {
+					slider.setPosition(GF::Mouse::getPosition(window).x, slider.getPosition().y);
+				}
+				if (event.type == GF::Event::MouseWheelMoved) {
+					if (box.getGlobalBounds().contains({ slider.getPosition().x + event.mouseWheel.delta * factor, slider.getPosition().y }))
+						slider.move(event.mouseWheel.delta * factor, 0);
+				}
+			}
+		}
+	}
+
+	void Slider<GF::Circle>::draw(sf::RenderTarget & renderer, const sf::RenderStates state) const
+	{
+		renderer.draw(line);
+		renderer.draw(slider);
+	}
+	void Slider<GF::Rectangle>::draw(sf::RenderTarget & renderer, const sf::RenderStates state) const
+	{
+		renderer.draw(slider);
+		renderer.draw(box);
 	}
 }
