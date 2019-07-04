57ca00feee363f80bf58989070e7412512f6108a
Miguel-Fr <miguel_franca@outlook.com>
2019-07-04 12:27:54 +0100

    events can now show their appropriate message on console

--- Game Framework/headers/sfml/Event.h
+++ Game Framework/headers/sfml/Event.h
@@ -60,6 +60,95 @@ namespace GF {
 			Count
 		};
 
+		void showMessage() {
+			switch (type)
+			{
+			case GF::Event::Closed:
+				std::cout << "closed" << std::endl;
+				break;
+			case GF::Event::Resized:
+				std::cout << "rezised" << std::endl;
+				break;
+			case GF::Event::LostFocus:
+				std::cout << "lost focus" << std::endl;
+				break;
+			case GF::Event::GainedFocus:
+				std::cout << "gained focus" << std::endl;
+				break;
+			case GF::Event::TextEntered:
+				std::cout << "text entered Key: " << (char)key.code << std::endl;
+				break;
+			case GF::Event::KeyPressed:
+				std::cout << "keypressed Key: " << key.code << std::endl;
+				break;
+			case GF::Event::KeyReleased:
+				std::cout << "key realesed Key: " << key.code <<  std::endl;
+				break;
+			case GF::Event::MouseWheelMoved:
+				std::cout << "mouse wheel moved Position: (" << mouseWheel.x << "," << mouseWheel.y << ")\tDelta: " << mouseWheel.delta << std::endl;
+				break;
+			case GF::Event::MouseWheelScrolled:
+				std::cout << "mouse wheel scrooled Position: (" << mouseWheel.x << "," << mouseWheel.y << ")\tDelta: " << mouseWheel.delta << std::endl;
+				break;
+			case GF::Event::MouseButtonPressed:
+				std::cout << "mouse button pressed Position: (" << mouseButton.x << "," << mouseButton.y << ")\tButton: " << mouseButton.button << std::endl;
+				break;
+			case GF::Event::MouseButtonReleased:
+				std::cout << "mouse button released Position: (" << mouseButton.x << "," << mouseButton.y << ")\tButton" << mouseButton.button << std::endl;
+				break;
+			case GF::Event::MouseMoved:
+				std::cout << "mouse moved Position: (" << mouseButton.x << "," << mouseButton.y << ")" << std::endl;
+				break;
+			case GF::Event::MouseEntered:
+				std::cout << "mouse entered screen Position: (" << mouseButton.x << "," << mouseButton.y << ")" << std::endl;
+				break;
+			case GF::Event::MouseLeft:
+				std::cout << "mouse left screen Position: (" << mouseButton.x << "," << mouseButton.y << ")" << std::endl;
+				break;
+			case GF::Event::JoystickButtonPressed:
+				std::cout << "joystick button pressed ID: " << joystickButton.joystickId << "\tButton: " << joystickButton.button << std::endl;
+				break;
+			case GF::Event::JoystickButtonReleased:
+				std::cout << "joystick button released ID: " << joystickButton.joystickId << "\tButton: " << joystickButton.button << std::endl;
+				break;
+			case GF::Event::JoystickMoved:
+				std::cout << "joystick moved ID: " << joystickMove.joystickId << "\tAxis: "<< joystickMove.axis << "\tPosition" << joystickMove.position << std::endl;
+				break;
+			case GF::Event::JoystickConnected:
+				std::cout << "joystick connected ID: " << joystickConnect.joystickId << std::endl;
+				break;
+			case GF::Event::JoystickDisconnected:
+				std::cout << "joystick disconnected ID: " << joystickConnect.joystickId << std::endl;
+				break;
+			case GF::Event::TouchBegan: // unsupported event
+				std::cout << "Touch began Finger: " << touch.finger << "\tPosition: (" << touch.x << "," << touch.y << ")" << std::endl;
+				break;
+			case GF::Event::TouchMoved:
+				std::cout << "Touch mouved" << "\tPosition: (" << touch.x << "," << touch.y << ")" << std::endl;
+				break;
+			case GF::Event::TouchEnded:
+				std::cout << "Touch ended Finger: " << touch.finger << "\tPosition: (" << touch.x << "," << touch.y << ")" << std::endl;
+				break;
+			case GF::Event::SensorChanged:
+				std::cout << "sensor changed Type: " << sensor.type << "\tPosition: (" << sensor.x << "," << sensor.y << "," << sensor.z << ")" << std::endl;
+				break;
+			case GF::Event::LeftMouseDoubleClickedEvent:
+				std::cout << "left double clicked Button: " << mouseButton.button << std::endl;
+				break;
+			case GF::Event::LeftMouseClickedOnceEvent:
+				std::cout << "left clicked once Button: " << mouseButton.button << std::endl;
+				break;
+			case GF::Event::RightMouseDoubleClickedEvent:
+				std::cout << "right double clicked Button: " << mouseButton.button << std::endl;
+				break;
+			case GF::Event::RightMouseClickedOnceEvent:
+				std::cout << "right clicked once Button: " << mouseButton.button << std::endl;
+				break;
+			default:
+				break;
+			}
+		}
+
 		EventType type;
 	};
 
