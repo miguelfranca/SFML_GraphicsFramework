c7ee9460370ab1b44a6fb922d3e362d3de86036e
MykeFr <myke.fr66@gmail.com>
2020-04-15 18:07:17 +0100

    ProgressBar

--- Game_Framework/Tools.h
+++ Game_Framework/Tools.h
@@ -3,6 +3,7 @@
 #include "Tools/Clock.hpp"
 #include "Tools/FileParser.hpp"
 #include "Tools/Instrumentor.h"
+#include "Tools/ProgressBar.hpp"
 #include "Tools/Matrix.hpp"
 #include "Tools/Random.hpp"
 #include "Tools/Tools.hpp"
